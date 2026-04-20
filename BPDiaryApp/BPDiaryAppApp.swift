053ebd17af1961855906b167e471a09d4ff72629
Nagina Yulia <cardsystem@mail.ru>
2026-04-20 11:20:38 +0300

    Initial Commit

---
+++ BPDiaryApp/BPDiaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPDiaryAppApp.swift
+//  BPDiaryApp
+//
+//  Created by Nagina Yulia on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct BPDiaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
